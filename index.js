e376b85d45133dfe88d3fb50f4144ebff19a1c8b
nnh <7bmzkr7h@sina.com>
2018-03-08 00:03:22 +0800

    1.完成一部分接口处理

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ const aggregator = new (require('./lib/aggregator'))(socket_server);
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
 
-http.listen(3100, () => log.info('listening on 3100'));
+http.listen(3100, () => log.info('listening on 3200'));
